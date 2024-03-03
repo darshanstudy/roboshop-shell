3226026e234eabdd027521c3db5d8904eeae1759
darshanstudy <nhari51989@gmail.com>
2024-03-04 02:02:48 +0530

    iam practicing java

--- shipping.sh
+++ shipping.sh
@@ -84,7 +84,7 @@ dnf install mysql -y &>> $LOGFILE
 
 VALIDATE $? "install MySQL client"
 
-mysql -h mysql.daws76s.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+mysql -h 172.31.6.52 -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
 
 VALIDATE $? "loading shipping data"
 
