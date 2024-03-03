1d8e77722622c9317600be5d9b1b6ef233533796
darshanstudy <nhari51989@gmail.com>
2024-03-04 01:35:12 +0530

    iam practicing java

--- catalogue.sh
+++ catalogue.sh
@@ -70,7 +70,7 @@ npm install  &>> $LOGFILE
 VALIDATE $? "Installing dependencies"
 
 # use absolute, because catalogue.service exists there
-cp  /c/Users/User/repos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+cp  /home/centos/root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
 
 VALIDATE $? "Copying catalogue service file"
 
